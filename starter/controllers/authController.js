a1dacc99a5827461b28244e2f609005fba141c10
Yusuff Joseph <joseph.yusuff12@gmail.com>
2023-04-21 13:49:17 +0100

    User Restrictions: Limiting actions to admin

--- starter/controllers/authController.js
+++ starter/controllers/authController.js
@@ -100,3 +100,17 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
+// User roles and permissions for deleting tours
+// eslint-disable-next-line arrow-body-style
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    // roles ['admin', 'lead-guide'], rple='user'
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action', 403)
+      ); //403 = forbidden
+    }
+    next();
+  };
+};
--- starter/models/userModel.js
+++ starter/models/userModel.js
@@ -11,6 +11,11 @@ const userSchema = new mongoose.Schema({
     required: [true, 'Please tell us your name'], //
   },
   photo: String,
+  role: {
+    type: String,
+    enum: ['user', 'guide', 'lead-guide', 'admin'],
+    default: 'user',
+  },
   email: {
     type: String,
     required: [true, 'Please provide your email'],
