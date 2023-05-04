23e0b6e23e43ed6afcb17cb514fc43a99948e3eb
Yusuff Joseph <joseph.yusuff12@gmail.com>
2023-05-04 22:19:00 +0100

    Logging in users with our API - part 3

--- starter/public/js/index.js
+++ starter/public/js/index.js
@@ -1,20 +1,13 @@
 /* eslint-disable */
+import 'regenerator-runtime/runtime';
+import 'core-js';
 import '@babel/polyfill';
-import { displayMap } from './mapbox';
+import { displayMap } from './leaflet';
 import { login } from './login';
-import { logout } from '/login';
-import { updateSettings } from './updateSettings';
-import { signup } from './signup';
 
 // DOM ELEMENTS
-const mapBox = document.getElementById('map');
-const loginForm = document.querySelector('.form--login');
-const logOutBtn = document.querySelector('.nav__el--logout');
-const userDataForm = document.querySelector('.form-user-data');
-const userPasswordForm = document.querySelector('.form-user-password');
-const signupForm = document.querySelector('.form--signup');
-
-// VALUES
+const mapBox = document.getElementById(`map`);
+const loginForm = document.querySelector(`.form`);
 
 // DELEGATION
 if (mapBox) {
@@ -25,56 +18,8 @@ if (mapBox) {
 if (loginForm) {
   loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+    const email = document.getElementById(`email`).value;
+    const password = document.getElementById(`password`).value;
     login(email, password);
-  });
-}
-
-if (userDataForm) {
-  userDataForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    document.querySelector('.btn--save-user-data').textContent = 'Updating ...';
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-
-    updateSettings({ name, email }, 'data');
-
-    document.querySelector('.btn--save-user-data').textContent =
-      'Save settings';
-  });
-}
-
-if (userPasswordForm) {
-  userPasswordForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    document.querySelector('.btn--save-password').textContent = 'Updating ...';
-    const passwordCurrent = document.getElementById('password-current').value;
-    const password = document.getElementById('password').value;
-    const passwordConfirm = document.getElementById('password-confirm').value;
-
-    await updateSettings(
-      { passwordCurrent, password, passwordConfirm },
-      'password'
-    );
-
-    document.querySelector('.btn--save-password').textContent = 'Save password';
-    document.getElementById('password-current').value =
-      document.getElementById('password').value =
-      document.getElementById('password-confirm').value =
-        '';
-  });
-}
-
-if (logOutBtn) logOutBtn.addEventListener('click', logout);
-
-if (signupForm) {
-  signupForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('password-confirm').value;
-    signup(name, email, password, confirmPassword);
   });
 }
--- starter/public/js/leaflet.js
+++ starter/public/js/leaflet.js
@@ -1,29 +1,28 @@
 /* eslint-disable */
-const locations = JSON.parse(document.getElementById(`map`).dataset.locations);
-console.log(locations);
+export const displayMap = (locations) => {
+  const map = L.map('map', { zoomControl: false });
 
-const map = L.map('map', { zoomControl: false });
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    crossOrigin: '',
+  }).addTo(map);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  crossOrigin: '',
-}).addTo(map);
+  map.setView([0, 0], 0);
+  // create an empty array to store each location coordinates as it is looped over then effevt the leaflet marker
+  const points = [];
+  locations.forEach((loc) => {
+    points.push([loc.coordinates[1], loc.coordinates[0]]);
+    L.marker([loc.coordinates[1], loc.coordinates[0]])
+      .addTo(map)
+      .bindPopup(`<p>Day ${loc.day}: ${loc.description}</p>`, {
+        autoClose: false,
+      })
+      .openPopup();
+  });
 
-map.setView([0, 0], 0);
-// create an empty array to store each location coordinates as it is looped over then effevt the leaflet marker
-const points = [];
-locations.forEach((loc) => {
-  points.push([loc.coordinates[1], loc.coordinates[0]]);
-  L.marker([loc.coordinates[1], loc.coordinates[0]])
-    .addTo(map)
-    .bindPopup(`<p>Day ${loc.day}: ${loc.description}</p>`, {
-      autoClose: false,
-    })
-    .openPopup();
-});
+  const bounds = L.latLngBounds(points).pad(0.5);
+  map.fitBounds(bounds);
 
-const bounds = L.latLngBounds(points).pad(0.5);
-map.fitBounds(bounds);
-
-map.scrollWheelZoom.disable();
+  map.scrollWheelZoom.disable();
+};
