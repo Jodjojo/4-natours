958a3cd0ef92a93aa24c0ccf06018552ae2460cb
Yusuff Joseph <joseph.yusuff12@gmail.com>
2023-05-12 16:47:20 +0100

    Modeling the Bookings and linking to user and tour

---
+++ starter/models/bookingModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+  // We must keep a reference of this booking schema to a user and a tour booked each time
+  tour: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Tour',
+    required: [true, 'Booking must belong to a tour'],
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'user',
+    required: [true, 'Booking must belong to a user'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'Booking must have a price'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  paid: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+// Automate populating the tour name and user whenever there is a query
+bookingSchema.pre(/^find/, function (next) {
+  this.populate('user').populate({
+    path: 'tour',
+    select: 'name',
+  });
+});
+
+const Booking = mongoose.model('Booking', bookingSchema);
+
+module.exports = Booking;
